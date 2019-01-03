ff68e49addc25cba1bfbb17abdbd70a3f6e30c0b
rkmad <rmadisetti3@gatech.edu>
2019-01-03 17:34:58 -0500

    Updates

---
+++ precache-manifest.15926f9327c368d997b98f04b9ac611b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7701815b74baf89f84",
+    "url": "/recipeapp/static/js/runtime~main.af770181.js"
+  },
+  {
+    "revision": "7b19f2ef9b45be27816c",
+    "url": "/recipeapp/static/js/main.7b19f2ef.chunk.js"
+  },
+  {
+    "revision": "a44f0c0078e70b51841d",
+    "url": "/recipeapp/static/js/1.a44f0c00.chunk.js"
+  },
+  {
+    "revision": "7b19f2ef9b45be27816c",
+    "url": "/recipeapp/static/css/main.ab2ac35a.chunk.css"
+  },
+  {
+    "revision": "3b7e6d0ca6edb51e028b32d2652de8e0",
+    "url": "/recipeapp/index.html"
+  }
+];
